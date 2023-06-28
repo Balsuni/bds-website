6dd48e3d8e4774359fb933c5561c5a4b24f6e0b2
dorteks <ayodelealayemi@gmail.com>
2023-06-28 02:18:56 +0100

    chore:project layout setup

---
+++ src/app/layout/navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row px-10 py-5 items-center justify-between bg-darkblue-300 ">
+      <h1>Navbar Here</h1>
+      <h1>Navbar Here</h1>
+      <h1>Navbar Here</h1>
+      <h1>Navbar Here</h1>
+      <h1>Navbar Here</h1>
+      <h1>Navbar Here</h1>
+      <h1>Navbar Here</h1>
+      <h1>Navbar Here</h1>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      darkblue: {
+        50: "#EDF2FF",
+        100: "#B1B8C8",
+        200: "#8B95AD",
+        300: "#647191",
+        400: "#3D4E76",
+        500: "#162A5A",
+        600: "#12234B",
+        700: "#0F1C3C",
+        800: "#0B152D",
+        900: "#091124",
       },
     },
   },
   plugins: [],
-}
+};
